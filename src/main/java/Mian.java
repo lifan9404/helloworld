727f23a3587342ff9c9a3af319ee3da91cae7488
LF <15802012880@163.com>
2020-06-11 22:15:27 +0800

    init

---
+++ src/main/java/Mian.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (C), 2015-2022,
+ * FileName: Mian
+ * Author:   lifan
+ * Date:     2020/6/11
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+public class Mian {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
